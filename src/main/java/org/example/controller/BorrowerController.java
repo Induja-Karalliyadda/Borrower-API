85e8f97eec83f50c476efd0c30257196c58d80a8
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-02-27 08:16:41 +0530

    add borrower method completed

--- src/main/java/org/example/controller/BorrowerController.java
+++ src/main/java/org/example/controller/BorrowerController.java
@@ -3,6 +3,8 @@ package org.example.controller;
 import org.example.dto.Borrower;
 import org.example.service.BorrowerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,8 @@ public class BorrowerController {
     @Autowired
     BorrowerService service;
 
-    public void addBorrower(Borrower borrower){
-
+    @PostMapping
+    public void addBorrower(@RequestBody Borrower borrower){
+        service.addBorrower(borrower);
     }
 }
