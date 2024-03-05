4500f07ecdd1a495018e812004501b22682c675d
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-03-05 07:24:04 +0530

    edite some modification

--- src/main/java/org/example/controller/BorrowerController.java
+++ src/main/java/org/example/controller/BorrowerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/borrower")
+@CrossOrigin
 public class BorrowerController {
 
     final BorrowerService service;
